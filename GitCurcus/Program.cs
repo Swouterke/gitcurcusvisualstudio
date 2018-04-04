2e57c646fa7c260d536b0b4c10a7bb7f583b7a59
Swouterke <38066476+Swouterke@users.noreply.github.com>
2018-04-04 10:57:14 +0200

    Update Program.cs

--- GitCurcus/Program.cs
+++ GitCurcus/Program.cs
@@ -10,7 +10,7 @@ namespace GitCurcus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo iedereen");
+            Console.WriteLine("Hallo allemaal");
         }
     }
 }
